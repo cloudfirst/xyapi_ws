6baaa34c5364bef47cb67f604ce76cb3ecd83140
cloudfirst <4167023@qq.com>
2018-11-19 18:02:06 +0800

    add code to install opencv 4.0 and tesseract 4.0

--- xyapi/wsapi.py
+++ xyapi/wsapi.py
@@ -6,6 +6,8 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
 )
 from werkzeug.exceptions import abort
+from sinobotocr.cv2_helper import *
+from sinobotocr.tesseract_helper import *
 
 bp = Blueprint('wsapi', __name__, url_prefix='/api/1.0')
 
@@ -17,21 +19,31 @@ def load_logged_in_user():
 
 @bp.route('/get/data', methods=['POST'])
 def get_data_from_pdf():
-    response = []
     if request.method == 'POST':
+        ret = {}
         file_name = request.form['datafile']
         full_path = os.path.join(BASE_DIR, file_name) 
         name, extention = os.path.splitext(file_name) 
         if extention == ".pdf" and os.path.exists(full_path):
             # start to process
-            ret = {}
-            ret['filename'] = file_name
-            ret['heji']     = '123.45'
-            ret['ztgz']     = u"钢筋混泥土"
+            ret['filename']  = file_name
+            ret['heji1']     = '123.45'
+            ret['heji2']     = '34.56'
+            ret['ztgz']      = u"鋼筋混凝土造"
             ret['confident'] = 0.8
-            response.append(ret)
-            return jsonify(results=response)
+            ret['Status']    = "OK"
+            ret['ErrDesc']   = ""
         else:
             abort(400, "invalid file name: %s." % file_name)
+            # start to process
+            ret['filename']  = file_name
+            ret['heji1']     = ''
+            ret['heji2']     = ''
+            ret['ztgz']      = u""
+            ret['confident'] = 0
+            ret['Status']    = "FAIL"
+            ret['ErrDesc']   = "invalid file name: %s." % file_name
+        
+        return jsonify(results=ret)
 
     
